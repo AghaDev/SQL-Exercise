771cdcce5668ec5f87384a11d15939124107f739
AghaDev <mohamadagha2@gmail.com>
2023-09-18 12:17:45 +0300

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,22 @@
+1)SELECT name FROM students
+
+2)SELECT * FROM students
+WHERE Age>30
+
+3)SELECT name FROM students
+WHERE Gender='F' AND Age=30
+
+4)SELECT Points FROM students
+WHERE name='Alex'
+
+5)INSERT INTO students
+(Name,Age,Gender,Points)
+VALUES
+("Mohammad",27,'M',700);
+
+6)UPDATE students SET Points = 500 WHERE name = "Basma"
+
+7)UPDATE students SET Points = 150 WHERE name = "Alex"
+
+8)
+
